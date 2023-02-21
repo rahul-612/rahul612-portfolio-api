b9fdda295766719f11d3801e7142d346c9686b07
rahul <rk785164@gmail.com>
2023-02-22 01:00:44 +0530

    bug fixed

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use(user);
 
 // }
 
-app.use(express.static(path.join(__dirname,'../client/build')));
+// app.use(express.static(path.join(__dirname,'../client/build')));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../client/build/index.html'))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../client/build/index.html'))
+// })
 app.listen(port,()=>{
     console.log(`Server is running at port ${port}`)
 })
